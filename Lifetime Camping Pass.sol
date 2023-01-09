86528a767b777e4339f01ad3ad5b84b0ec2e6a6b
Boone Bergsma <b.bergsma@wethinkitmattersinc.com>
2023-01-08 21:15:14 -0800

    Create Lifetime Camping Pass.sol

---
+++ Lifetime Camping Pass.sol
@@ -0,0 +1,9 @@
+{"name":"REGEN Nations 14 Days a Year Lifetime Camping Pass",
+    "title":"REGEN Nations 2023 Arizona Regen Campout – Solar Panel Pass",
+    "description":"A lifetime pass to camp up to 14 days a year for unlimited years at any REGEN Nations properties. REGEN Nations offers a capped number of Camping Pass NFTs based on total land owned. Camping Pass NFT holders have access to camp a certain number of days at any of our properties at any time during the year. Lifetime Camping Pass NFT holders are able to make reservations to use any REGEN Nations property for the amount of days allotted in the terms of the NFT. Lifetime Camping Pass Holders can use all their allotted days at once on one property, or for a couple of days at multiple properties at different times in the year. You can use your Camping Pass NFT to camp on properties during Annual Campout events, Lifetime Camping Pass NFT it is not a ticket to the events and workshops. Lifetime Camping Pass NFTs never expire and the days available start over at the beginning of each year. They can be transferred, traded, or sold, and whoever the holder is they can make reservations.",
+    "image":"coming soon",
+    "categories":["Land Use Rights", "Camping Pass"],
+    "attributes":[
+        {"trait_type":"Land Use Rights","value":"Fourteen day/nights of camping use for up to four people."},
+        {"trait_type":"Year","value":"Perpetual"},
+        {"trait_type":"terms","value":"Whoever hold this NFT gains access to camp on REGEN NAtion properties"}]}
